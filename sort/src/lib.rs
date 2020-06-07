d99c117275040bd1bbffbbfde09abc4fdc8354f3
zmant <zmant724@aliyun.com>
2020-06-07 18:13:29 +0800

    Implment select sort in rust

--- sort/src/lib.rs
+++ sort/src/lib.rs
@@ -2,3 +2,4 @@
 mod quick_sort;
 mod bubble_sort;
 mod insert_sort;
+mod select_sort;
--- sort/src/select_sort.rs
+++ sort/src/select_sort.rs
@@ -1,44 +1,44 @@
+use std::process::id;
 
-fn bubble_sort<T: Ord>(arr: &mut [T]) {
+fn select_sort<T: Ord>(arr: &mut [T]) {
     for i in 0..arr.len() {
+        let mut idx = i;
         for j in i..arr.len() {
-            if arr[j] < arr[i] {
-                arr.swap(i, j);
+            if arr[j] < arr[idx] {
+                idx = j;
             }
         }
+        arr.swap(idx, i);
     }
 }
 
 mod test {
-
-    use super::bubble_sort;
-
+    use super::select_sort;
+    
     #[test]
     fn test_sort1() {
-
         let mut nums = vec![9, 3, 67, 4, 1, 89, -234, 5, -345, 56, 123, 8, 1, 9, 55];
         println!("Before sort: {:?}", nums);
-        bubble_sort(&mut nums);
+        select_sort(&mut nums);
         println!("After sort: {:?}\n", nums);
         assert_eq!(nums, vec![-345, -234, 1, 1, 3, 4, 5, 8, 9, 9, 55, 56, 67, 89, 123]);
 
         let mut nums1 = vec![5, 4, 2, 3, 1];
         println!("Before sort: {:?}", nums1);
-        bubble_sort(&mut nums1);
+        select_sort(&mut nums1);
         println!("After sort: {:?}\n", nums1);
         assert_eq!(nums1, vec![1, 2, 3, 4, 5]);
 
         let mut nums2 = vec![5, 4, 3, 2, 1];
         println!("Before sort: {:?}", nums2);
-        bubble_sort(&mut nums2);
+        select_sort(&mut nums2);
         println!("After sort: {:?}\n", nums2);
         assert_eq!(nums2, vec![1, 2, 3, 4, 5]);
 
         let mut strs = vec!["Obama", "Trump", "Bush2", "Clinton", "Bush1", "Reagan", "Carter", "Ford", "Nixon", "Hillary"];
         println!("Before sort: {:?}", strs);
-        bubble_sort(&mut strs);
+        select_sort(&mut strs);
         println!("After sort: {:?}\n", strs);
         assert_eq!(strs, vec!["Bush1", "Bush2", "Carter", "Clinton", "Ford", "Hillary", "Nixon", "Obama", "Reagan", "Trump"]);
     }
-
 }
